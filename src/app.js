75acb82ee3d513578f22d49e3be90114bb87682e
1712832wine <nguyenhuutri31081999nht@gmail.com>
2022-05-07 22:24:24 +0700

    b1

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.engine('hbs', exphbs.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+app.get('/other', function (req, res) {
+    res.render('other', { layout: 'layout1' });
+});
+
+
+app.listen(3000);
